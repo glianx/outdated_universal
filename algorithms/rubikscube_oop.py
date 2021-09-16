b4519ff8a04f5957a5872a1243183a240a787eef
Gordon Liang <gordon.liang@ucc.on.ca>
2021-09-16 08:07:33 -0400

    sep16

--- algorithms/rubikscube_oop.py
+++ algorithms/rubikscube_oop.py
@@ -153,6 +153,20 @@ def custom_scramble():
 
         input_ = input().upper()
 
+def random_scramble():
+    os.system('clear')
+
+    scramble_move_list = ["R"]
+    for i in range(7):
+        scramble_move_list.append(random.choice([x for x in real_moves if x[0] != scramble_move_list[-1][0]]))
+    scramble = ' '.join(scramble_move_list)
+
+    # scramble = "R F R2 L D2 F2 L F'"
+
+    execute(scramble)
+    render()
+    print('scramble:',scramble)
+
 def render_exit():
     render()
     runtime = time.time() - start
@@ -181,50 +195,201 @@ blankface = Face(blank)
 real_moves = "U U' U2 R R' R2 L L' L2 F F' F2 D D' D2 B B' B2".split()
 moves = "N U U' U2 R R' R2 L L' L2 F F' F2 D D' D2 B B' B2".split()
 
-
-os.system('clear')
-
-scramble_move_list = ["R"]
-for i in range(20):
-    scramble_move_list.append(random.choice([x for x in real_moves if x[0] != scramble_move_list[-1][0]]))
-# scramble = ' '.join([random.choice(real_moves) for x in range(20)])
-scramble = ' '.join(scramble_move_list)
-execute(scramble)
-render()
-print('scramble:',scramble)
-
 # custom_scramble()
+random_scramble()
+
+tperm = "R U R' U' R' F R2 U' R' U' R U R' F'"
+uaperm = "F2 U L R' F2 L' R U F2"
+ubperm = "F2 U' L R' F2 L' R U' F2"
+def pll_solved():
+    return [fface.ul,fface.u,fface.ur,lface.ul,lface.u,lface.ur,rface.ul,rface.u,rface.ur,bface.ul,bface.u,bface.ur] == [blue,blue,blue,orange,orange,orange,red,red,red,green,green,green]
+
+def headlights_solved():
+    return uface.ul == uface.ur and lface.ul == lface.ur and rface.ul == rface.ur and bface.ul == bface.ur
+
+def pll():
+    print('pll')
+    for move1 in ["N",tperm]: # create headlights
+        execute(move1)
+        for move2 in ["N","U","U'","U2"]: # turn uface
+            execute(move2)
+            for move3 in ["N",tperm]: # create all headlights
+                execute(move3)
+                if headlights_solved():
+                    for move4 in ["N","U","U'","U2"]: # turn uface
+                        execute(move4)
+                        for move5 in ["N",uaperm,ubperm]: # solve
+                            execute(move5)
+                            for move6 in ["N","U","U'","U2"]: # solve
+                                execute(move6)
+                                if pll_solved():
+                                    print(move1,move2,move3)
+                                    print(move4,move5)
+                                    render()
+                                    print('runtime:',time.time() - start)
+                                    exit()
+                                undo(move6)
+                            undo(move5)
+                        undo(move4)
+                undo(move3)
+            undo(move2)
+        undo(move1)
+
+
+def yellow_cross_solved():
+    return [uface.u,uface.l,uface.r,uface.d] == [yellow,yellow,yellow,yellow]
+
+def yellow_face_solved():
+    return [uface.u,uface.l,uface.r,uface.d,uface.ul,uface.ur,uface.dl,uface.dr] == [yellow for _ in range(8)]
+
+def oll_solve_cross():
+    print('full yellow face')
+    for move1 in ["N","U","U'","U2"]: 
+        execute(move1)
+        for alg1 in ["N","R U R' U R U2 R'","R U2 R' U' R U' R'","R2 D R' U2 R D' R' U2 R'","R' F R B' R' F' R B","R' F' L F R F' L' F","R U2 R' U' R U R' U' R U' R'","R U2 R2 U' R2 U' R2 U2 R"]:
+            execute(alg1) # sune, antisune, superman, bow, fatboy, fastcross, slowcross
+            if yellow_face_solved():
+                print(move1,alg1)
+                render()
+                pll()
+                exit()
+            undo(alg1)
+        undo(move1)
+
+def oll_create_cross():
+    # create cross
+    print('creating yellow cross')
+    for move1 in ["N","F R U R' U' F'"]: # solve dot
+        execute(move1)
+        for move2 in ["N","U","U'","U2"]: # turn uface
+            execute(move2)
+            for move3 in ["N","F U R U' R' F'","F R U R' U' F'"]: # solve L / bar
+                execute(move3)
+                if yellow_cross_solved():
+                    print(move1,move2,move3)
+                    render()
+                    oll_solve_cross()
+                undo(move3)
+            undo(move2)
+        undo(move1)
+
+
+
+    
+
+def winter_summer_positions():
+    return [[fface.ur,uface.dr,uface.r,rface.ul,rface.u],[fface.ul,lface.ur,lface.u,uface.dl,uface.l],[fface.ur,uface.dr,lface.u,rface.ul,uface.l],[fface.ul,lface.ur,uface.r,uface.dl,rface.u]]
+
+def f2lslot(i):
+    return [[dface.ur,fface.dr,fface.r,rface.dl,rface.l],[dface.ul,lface.dr,lface.r,fface.dl,fface.l],[dface.dl,bface.dr,bface.r,lface.dl,lface.l],[dface.dr,rface.dr,rface.r,bface.dl,bface.l]][i]
+
+f2lpair_colours = [[blue,red],[orange,blue],[green,orange],[red,green]]
+
+def prev_f2lpairs_done(i):
+    for x in range(i):
+        col1,col2 = f2lpair_colours[i]
+        if [white,col1,col1,col2,col2] != f2lslot(x):
+            return False
+    return True
+
+
+def f2l(i):
+    if i == 4: 
+        # render()
+        oll_create_cross()
+        return()
+    for move1 in moves:
+        execute(move1)
+        for move2 in moves:
+            execute(move2)
+            for move3 in moves:
+                execute(move3)
+                for move4 in moves:
+                    execute(move4)
+                    for move5 in ["N","U","U'","U2"]:
+                        execute(move5)
+                        col1,col2 = f2lpair_colours[i]
+                        if [white,col1,col1,col2,col2] in winter_summer_positions():
+                            if cross_solved(): # and prev_f2lpairs_done(i):
+                                undo(move5)
+
+                                # render()
+                                # print(move1,move2,move3,move4)
+                                
+                                for move6 in moves:
+                                    execute(move6)
+                                    for move7 in moves:
+                                        execute(move7)
+                                        for move8 in moves:
+                                            execute(move8)
+                                            for move9 in moves:
+                                                execute(move9)
+                                                if [white,col1,col1,col2,col2] == f2lslot(i):
+                                                    if cross_solved(): # and prev_f2lpairs_done(i):
+                                                        for move in [move1,move2,move3,move4,move6,move7,move8,move9]:
+                                                            print(move, end = ' ')
+                                                        print()
+
+                                                        render()
+                                                        f2l(i+1)
+                                                        exit()
+                                                undo(move9)
+                                            undo(move8)
+                                        undo(move7)
+                                    undo(move6)
+                        undo(move5)
+                    undo(move4)
+                undo(move3)
+            undo(move2)
+        undo(move1)
+
+
+
+
+
+
+# # ====================================================================================
+
+# # cross
 
 unsolvable_optimizations = 0
+positions_analyzed = 0
 start = time.time()
 
 for move1 in moves:
     execute(move1)
     for move2 in moves:
+        if move2[0] == move1[0] and move2 != 'N': continue
         execute(move2)
         for move3 in moves:
+            if move3[0] == move2[0] and move3 != 'N': continue
             execute(move3)
             for move4 in moves:
+                if move4[0] == move3[0] and move4 != 'N': continue
                 execute(move4)
                 for move5 in moves:
+                    if move5[0] == move4[0] and move5 != 'N': continue
                     execute(move5)
                     if cross_solvable(5) != True:
                         undo(move5)
                         unsolvable_optimizations += 18 ** 3
                         continue
                     for move6 in moves:
+                        if move6[0] == move5[0] and move6 != 'N': continue
                         execute(move6)
                         if cross_solvable(6) != True:
                             undo(move6)
                             unsolvable_optimizations += 18 ** 2
                             continue
                         for move7 in moves:
+                            if move7[0] == move5[0] and move7 != 'N': continue
                             execute(move7)
                             if cross_solvable(7) != True:
                                 undo(move7)
                                 unsolvable_optimizations += 18 ** 1
                                 continue
                             for move8 in moves:
+                                if move8[0] == move7[0] and move8 != 'N': continue
                                 execute(move8)
                                 if cross_solved(): 
                                     print('cross: ',end = '')
@@ -235,7 +400,14 @@ for move1 in moves:
 
 
                                     print('unsolvable_optimizations:',unsolvable_optimizations)
-                                    render_exit()    
+                                    print('positions analyzed:', positions_analyzed)
+                                    # render_exit()  
+                                    render()
+                                    f2l(0)
+                                    exit()
+
+                                positions_analyzed += 1
+
                                 undo(move8)
                             undo(move7)
                         undo(move6)
--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,1 @@
-li = [0,1,2,3]
-li2 = li.copy()
-
-for i in range(4):
-    print(li)
-    li[i] = li2[i-1]
-print(li,li2)
-
-
+[fface.ul,uface.dl,rface.u,lface.ur,uface.r]
